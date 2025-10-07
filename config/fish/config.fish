cc36b25cda8dba349565fb05291f12a5c0eda77d
yongjun <1119272440@qq.com>
2025-10-07 16:41:15 +0800

    remove fzf and rbenv command from fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-fzf --fish | source
+# fzf --fish | source
 
 alias sby3i='ssh yongjun@10.98.36.218 -p 2222'
 alias sby3o='ssh yongjun@115.233.223.42 -p 20039'
@@ -32,7 +32,7 @@ end
 # <<< conda initialize <<<
 
 # Added by `rbenv init` on 2025年 4月 2日 星期三 14时54分59秒 CST
-status --is-interactive; and rbenv init - --no-rehash fish | source
+# status --is-interactive; and rbenv init - --no-rehash fish | source
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
