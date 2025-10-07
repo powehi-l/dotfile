33cdd7275aa8c9ba59fba40b26d4f4942ef7b785
yongjun <1119272440@qq.com>
2025-10-07 17:08:57 +0800

    add conda condition for macos and linux

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,9 +18,12 @@ alias vim=nvim
 
 export EDITOR=nvim
 
+set -l os_type (uname -s)
+
 switch (uname -s)
     case Linux
-        fish_add_path "~/.linuxbrew/bin"
+        fish_add_path ~/.linuxbrew/bin
+	fish_add_path ~/.linuxbrew/sbin
     case Darwin
         fish_add_path /opt/homebrew/bin
         fish_add_path /opt/homebrew/sbin
@@ -28,14 +31,26 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" hook $argv | source
+if test "$(uname)" = Linux
+	if test -f /home/yongjun/miniconda3/bin/conda
+	    eval /home/yongjun/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+	else
+	    if test -f "/home/yongjun/miniconda3/etc/fish/conf.d/conda.fish"
+	        . "/home/yongjun/miniconda3/etc/fish/conf.d/conda.fish"
+	    else
+	        set -x PATH "/home/yongjun/miniconda3/bin" $PATH
+	    end
+	end
 else
-    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH /opt/homebrew/Caskroom/miniconda/base/bin $PATH
-    end
+	if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+	    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" hook $argv | source
+	else
+	    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+	        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+	    else
+	        set -x PATH /opt/homebrew/Caskroom/miniconda/base/bin $PATH
+	    end
+	end
 end
 # <<< conda initialize <<<
 
